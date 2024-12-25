245137144bd0cf352637d5edbda29ae3be47d5bd
Saeed <saidakbarp@gmail.com>
2024-12-24 21:24:00 -0800

    Update rocm_flash_attn.py
    paged attention is not supported in GCN5.

--- vllm/attention/backends/rocm_flash_attn.py
+++ vllm/attention/backends/rocm_flash_attn.py
@@ -680,7 +680,7 @@ def _use_rocm_custom_paged_attention(qtype: torch.dtype, head_size: int,
                                      block_size: int, gqa_ratio: int,
                                      max_seq_len: int) -> bool:
     # rocm custom page attention not support on navi (gfx1*)
-    return (_ON_MI250_MI300 and not _ON_NAVI
+    return (_ON_MI250_MI300 and not _ON_NAVI and not _ON_GCN5
             and (qtype == torch.half or qtype == torch.bfloat16)
             and (head_size == 64 or head_size == 128)
             and (block_size == 16 or block_size == 32)
